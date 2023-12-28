d5e045db499f59b3fa01540bed4916fc200dd910
DecryptLife <benson.thomas2602@gmail.com>
2023-12-27 23:39:09 -0600

    Fetching data from websites

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # Selenium
 
-### 1. Opening BRAVE from python code###
+### 1. Opening BRAVE from python code
 
 - choose webdriver - requires further installation for some WebDrivers (Chrome, Firefox, Edge , etc)
 - store the webdriver to a browser => browser = webdriver.WEB_DRIVER_NAME
 - to open => browser.get(link)
+
+### Fetching elements from websites
+
+This part we are fetching information from [amazon website][https://www.amazon.com/s?i=fashion-mens&bbn=23610279011&rh=n%3A7141123011%2Cn%3A23610279011%2Cn%3A7147441011%2Cn%3A1040658%2Cn%3A1045558&s=exact-aware-popularity-rank&ref=s9_bw_cg_SBPWINDT_4d1_w]
+
+Displaying each item's title,price, image and product link
+
+- using find_elements(By.PARAMETERS(CSS_SELECTOR, XPATH, TAG_NAME and more)) we can target specific items on the page
+- once we get all the items and store it we can traverse the list and use the above function to target specific parts of the item
+- when we need something other than text use get_attribute("PARAMETER") to get that particular value
+- use try and catch so as to avoid interruption in case certain values are missing
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 barve_path = '/usr/bin/brave-browser'
 
@@ -7,4 +8,33 @@ options = Options()
 options. binary_location = barve_path
 
 browser = webdriver.Chrome(options=options)
-browser.get('http://selenium.dev/')
+browser.get('https://www.amazon.com/s?i=fashion-mens&bbn=23610279011&rh=n%3A7141123011%2Cn%3A23610279011%2Cn%3A7147441011%2Cn%3A1040658%2Cn%3A1045558&s=exact-aware-popularity-rank&ref=s9_bw_cg_SBPWINDT_4d1_w')
+
+elem_list = browser.find_element(By.CSS_SELECTOR, "div.s-main-slot.s-result-list.s-search-results.sg-row")
+
+items = elem_list.find_elements(By.XPATH, '//div[@data-component-type="s-search-result"]')
+
+
+for item in items:
+    title = item.find_element(By.TAG_NAME, 'h2').text
+
+    price = "No price provided"
+    image =  "No image provided"
+
+    link = item.find_element(By.CLASS_NAME, 'a-link-normal').get_attribute('href')
+    try:
+        price = item.find_element(By.CSS_SELECTOR, '.a-price').text.replace("\n",".")
+    except: 
+        pass
+
+
+    try:
+        image = item.find_element(By.CSS_SELECTOR,'.s-image').get_attribute("src")
+    except:
+        pass
+
+
+    print("Title: "+title)
+    print("Price: "+price)
+    print("Image: "+image)
+    print("LINK: "+link+"\n")
